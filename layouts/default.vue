92ebfd62ba09ca1a4d82561dd6e99e6e2c0a4f05
NigthBus <sngiagom@gmail.com>
2023-04-01 21:32:13 -0500

    Se crea la conexión con la API

---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <v-menu offset-y>
+        <v-list>
+          <v-list-item to="/profile">
+            <v-list-item-title>Mi perfil</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="closeSesion">
+            <v-list-item-title>Salir</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  beforeMount() {
+    this.loadAccount()
+  },
+  data() {
+    return {
+      fullname: "",
+      clipped: true,
+      fixed: false,
+      miniVariant: false,
+      title: 'MARVEL CHARACTERS',
+    }
+  },
+  methods: {
+    closeSesion() {
+      this.$router.push("/")
+    },
+    loadAccount() {
+      const account_str = localStorage.getItem("ACCOUNT")
+      if (account_str) {
+        try {
+          const account = JSON.parse(account_str)
+          this.fullname = `${account.firstname} ${account.lastname}`
+          //TODO: Validando el rol pendiente desde el API
+          if (account.role == 0) {
+            this.$router.push("/students/home")
+          } else {
+            this.$router.push("/home")
+          }
+        } catch (error) {
+          this.$router.push("/")
+        }
+      } else {
+        this.$router.push("/")
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>LOAD MARVEL CHARACTERS</h1>
+    
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      accounts: [],
+      loading: false, 
+      show: false
+    }
+  },
+  methods: {
+    loadAccounts() {
+      console.log('Cargar cuentas')
+      const url =
+        'https://gateway.marvel.com:443/v1/public/characters?ts=10&apikey=af468b3503603801817dfff31982be6b&hash=70f8e7fd7ba77a7d1ad086d52bc20bc6'
+        this.loading = true
+        this.$axios
+        .get(url)
+        .then((response) => {
+          const data = response.data
+          this.accounts = data.data.results
+          console.log(response)
+        })
+        .catch((error) => {
+          alert('Ha ocurrido un error al cargar los personajes')
+          console.log(error)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+  },
+}
+</script>
+
+<style></style>
